6fbf95d464eea72beddbde55aec33aafbf272079
Liver64 <olewald64@gmail.com>
2018-10-24 21:40:38 +0200

    update interface

--- webfrontend/html/output/interface.php
+++ webfrontend/html/output/interface.php
@@ -1,20 +1,20 @@
 <?php
 /**
-* Submodul: generisches Interface
+* Submodul: bidirectonal Interface
 *
 **/
 
 
 /**
-/* Funktion : receive_post_request --> Funktion zum Erhalten eines POST Requests von externen Programmen/Plugins
-/* @param: 	leer
+/* Funktion : receive_post_request --> Function to receive and validate incoming Requests
+/* @param: 	empty
 /*
-/* @return: array für weitere Verwendung
-/*			[0] Text für T2S
-/*			[1] greet=0 ohne Grußformel, greet=1 mit Grußformel 
+/* @return: array (
+/*			[0] Text for T2S (string)
+/*			[1] greet=0 w/o greeting, greet=1 with greeting (bolean)
 /**/
 
-function receive_post_request() {
+function process_post_request() {
 	
 	// http://thisinterestsme.com/receiving-json-post-data-via-php/
 	// http://thisinterestsme.com/sending-json-via-post-php/
@@ -22,25 +22,29 @@ function receive_post_request() {
 	
 	// Make sure that it is a POST request.
 	if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
-		LOGGING("T2S Interface ** Request method must be POST!", 3);
+		LOGERR("T2S Interface ** Request method must be POST!");
 		exit;
 	}
+	
 	// Make sure that the content type of the POST request has been set to application/json
 	$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 	if(strcasecmp($contentType, 'application/json') != 0){
-		LOGGING("T2S Interface ** Content type must be: application/json", 3);
+		LOGERR("T2S Interface ** Content type must be: application/json");
 		exit;
 	}
+	
 	// Receive the RAW post data.
 	$content = trim(file_get_contents("php://input"));
+	
 	// Attempt to decode the incoming RAW post data from JSON.
 	$decoded = json_decode($content, true);
+	
 	// If json_decode failed, the JSON is invalid.
 	if(!is_array($decoded)){
-		LOGGING("T2S Interface ** Received content contained invalid JSON!", 3);
+		LOGERR("T2S Interface ** Received content contained invalid JSON!");
 		exit;
 	}
-	LOGGING("T2S Interface ** POST request has been successful processed!", 5);
+	LOGOK("T2S Interface ** POST request has been successful processed!");
 	return ($decoded);
 }
 
@@ -54,18 +58,30 @@ function receive_post_request() {
 /**/	
 
 function jsonfile($filename)  {
-	global $volume, $MessageStorepath, $MP3path, $messageid, $time_start, $filename, $infopath, $config, $ttsinfopath, $filepath, $ttspath, $myIP, $plugindatapath, $lbhomedir, $psubfolder, $hostname, $fullfilename, $text, $textstring;
+	global $volume, $MessageStorepath, $MP3path, $messageid, $time_start, $filename, $infopath, $config, $ttsinfopath, $filepath, $ttspath, $myIP, $plugindatapath, $lbhomedir, $psubfolder, $hostname, $fullfilename, $text, $textstring, $duration;
 	
 	$filenamebatch = $ttsinfopath."".$fullfilename;
 	$ttspath = $MessageStorepath;
 	$filepath = $MessageStorepath."".$MP3path;
 	$ttsinfopath = LBPDATADIR."/".$infopath."/";
 	
+	// get details of MP3
+	// https://github.com/JamesHeinrich/getID3/archive/master.zip
+	require_once("bin/getid3/getid3.php");
+    $MP3filename = $MessageStorepath.$messageid.".mp3";
+	$getID3 = new getID3;
+    $file = $getID3->analyze($MP3filename);
+	#print_r($file);
+	$duration = round($file['playtime_seconds'] * 1000, 0);
+	$bitrate = $file['bitrate'];
+	$sample_rate = $file['mpeg']['audio']['sample_rate'];
+    #echo gmdate("H:i:s", $duration);
+	
 	LOGGING("filename of MP3 file: '".$filename."'", 5);
 	# prüft ob Verzeichnis für Übergabe existiert
 	$is_there = file_exists($ttsinfopath);
 	if ($is_there === false)  {
-		LOGGING("The interface folder seems not to be available!! System now try to create the 'share' folder.", 4);
+		LOGGING("The interface folder seems not to be available!! System now try to create the 'share' folder", 4);
 		mkdir($ttsinfopath);
 		LOGGING("Folder '".$ttsinfopath."' has been succesful created.", 5);
 	} else {
@@ -74,7 +90,7 @@ function jsonfile($filename)  {
 	# Löschen alle vorhandenen Dateien aus dem info folder
 	chdir($ttsinfopath);
 	foreach (glob("*.*") as $file) {
-		LOGGING("File: '".$file."' has been deleted from '".$infopath."' folder.",5);
+		LOGGING("File: '".$file."' has been deleted from '".$infopath."' folder",5);
 		#unlink($file);
 	}
 	$files = array();
@@ -88,6 +104,9 @@ function jsonfile($filename)  {
 						'full-server-URL' => $myIP."/".$plugindatapath."/".$psubfolder."/".$filename.".mp3",
 						'server-URL' => $myIP."/".$plugindatapath."/".$psubfolder."/",
 						'mp3-filename-MD5' => $filename,
+						'duration-ms' => $duration,
+						'bitrate' => $bitrate,
+						'sample-rate' => $sample_rate,
 						'text' => $textstring
 						);
 	} else {
@@ -98,22 +117,27 @@ function jsonfile($filename)  {
 						'full-server-URL' => $split[6]."/".$split[7]."/".$hostname."/tts/".$filename.".mp3",
 						'server-URL' => $split[6]."/".$split[7]."/".$hostname."/tts/",
 						'mp3-filename-MD5' => $filename,
+						'duration-ms' => $duration,
+						'bitrate' => $bitrate,
+						'sample-rate' => $sample_rate,
 						'text' => $textstring
 						);
 		} else {
-			LOGGING("USB devices and NetShares could not be interfaced!", 3);
+			LOGGING("USB devices could not be interfaced!", 3);
 			exit;
 		}
 	}
-	#print_r($files);
 	try {
 		File_Put_Array_As_JSON($filenamebatch, $files, $zip=false);
+		LOGGING("Information of processed T2S has been added to JSON file", 7);
 	} catch (Exception $e) {
 		LOGGING("JSON file could not be saved! Please check your system (permission, credentials, etc.)", 3);
 		exit;
 	}
 	LOGGING("MP3 file has been saved successful at '".$files['path']."'.", 6);
-	LOGGING("Information of processed T2S has been added to JSON file", 7);
+	LOGGING("JSON file '".$fullfilename."' has been successful saved in 'share' folder", 5);
+	#print_r($files);
+	return $files;
 	
 }
 
