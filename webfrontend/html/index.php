52a40e99d0948c917a0115b12a00dd4c0825fa4e
Liver64 <olewald64@gmail.com>
2018-11-04 09:26:31 +0100

    added custom error handler

--- webfrontend/html/index.php
+++ webfrontend/html/index.php
@@ -1,13 +1,22 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
-error_reporting(0);   					// turn off all errors/warnings/notice
-ini_set("display_errors", false);      
-ini_set('html_errors', false);			 
-
 require_once "loxberry_system.php";
 require_once "loxberry_log.php";
-$L = LBSystem::readlanguage("tts_all.ini");
+include "error.php";
 
+error_reporting(E_ALL);
+ini_set("display_errors", "off");
+define('ERROR_LOG_FILE', "$lbplogdir/text2speech.log");
+
+//calling custom error handler
+set_error_handler("handleError");
+
+// Testcases
+#print_r($arra); // undefined variable
+#print_r($dssdfdfgg); // undefined variable
+#include_once 'file.php'; // No such file or directory
+			 
+$L = LBSystem::readlanguage("tts_all.ini");
 require_once 'tts.php';
 ?>
